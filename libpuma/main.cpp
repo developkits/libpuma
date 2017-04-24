2464160d073a768c30e4215c145d571aa94d3972
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2017-04-25 01:01:25 +0200

    Initial Commit

---
+++ libpuma/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  libpuma
+//
+//  Created by Ibrahim Abdullah on 25/04/2017.
+//  Copyright © 2017 Ibrahim Abdullah. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
